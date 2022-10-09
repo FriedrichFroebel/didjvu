2a264e418411b6ee251f58fc6e35ab68a59143bd
FriedrichFroebel <FriedrichFroebel@users.noreply.github.com>
2022-10-09 16:46:13 +0200

    migrate to setup.py-based approach

---
+++ setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+from pathlib import Path
+
+
+ROOT_DIRECTORY = Path(__file__).parent.resolve()
+
+
+def get_version():
+    changelog = ROOT_DIRECTORY / 'doc' / 'changelog'
+    with open(changelog, mode='r') as fd:
+        line = fd.readline()
+    return line.split()[1].strip('()')
+
+
+setup(
+    name='didjvu',
+    description='DjVu encoder with foreground/background separation (Python 3 fork) ',
+    version=get_version(),
+    license='GNU GPL 2',
+    long_description=(ROOT_DIRECTORY / 'README.rst').read_text(encoding='utf-8'),
+    long_description_content_type='text/x-rst',
+    author='Jakub Wilk, FriedrichFröbel (Python 3)',
+    url='https://github.com/FriedrichFroebel/didjvu/',
+    packages=find_packages(
+        where='.',
+        exclude=['tests', 'tests.*', 'private', 'private.*']
+    ),
+    include_package_data=True,
+    python_requires=">=3.6, <4",
+    install_requires=[
+        'gamera>=4.0.0',
+        'Pillow',
+    ],
+    extras_require={
+        'dev': [
+            'coverage',
+            'flake8',
+            'pep8-naming',
+        ],
+        'docs': [
+            'docutils',
+            'pygments',
+        ]
+    },
+    entry_points={
+        'console_scripts': [
+            'didjvu=didjvu.__main__:main',
+        ],
+    },
+)
