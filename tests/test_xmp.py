e9102a199906526d337ce1d61926b37c98eb49d6
FriedrichFroebel <FriedrichFroebel@users.noreply.github.com>
2023-07-30 14:32:42 +0200

    Update test_xmp.py

--- tests/test_xmp.py
+++ tests/test_xmp.py
@@ -36,6 +36,8 @@ def import_backend(name):
     try:
         backend = importlib.import_module(mod_name)
     except ImportError as _import_error:
+        import traceback
+        traceback.print_stack()
         import_error = _import_error
 
         class Backend:
